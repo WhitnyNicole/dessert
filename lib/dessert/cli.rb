1e452f9ffeaeea60be4c552444ebd3ce60e51c91
Whitny Edwards <wne4e2@gmail.com>
2019-01-24 03:12:53 +0000

    start creating CLI file

--- lib/dessert/cli.rb
+++ lib/dessert/cli.rb
@@ -1,7 +1,26 @@
 class Dessert::CLI 
   
   def start #instance method
-    puts "hello world"
+    puts "Welcome to the Protein Bakery"
+    puts "What type of dessert would you like?"
+    puts "Cookies, Brownies, Blondies, Minis?"
+    puts "Type the number of your choice"
+    input = gets.strip.downcase
+    puts input
+    case input #case statement 
+      when "cookies"
+        puts "you selected cookies"
+      when "brownies" 
+        puts "you selected brownies"
+      when "blondies"
+        puts "you selected blondies"
+      when "minis"
+        puts "you selected minis"
+      when "exit"
+        puts "Goodbye"
+      else
+        puts "Invalid"
+    end 
   end 
   
 end 
