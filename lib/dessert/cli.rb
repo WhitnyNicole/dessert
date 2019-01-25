1d9799afbe3acd63b8ec131280c02983c2cd7c79
Whitny Edwards <wne4e2@gmail.com>
2019-01-25 01:27:38 +0000

    working on scraper

--- lib/dessert/cli.rb
+++ lib/dessert/cli.rb
@@ -6,18 +6,18 @@ class Dessert::CLI
     puts "Welcome to the Protein Bakery"
     puts "What type of dessert would you like?"
     puts "Cookies, Brownies, Blondies, Minis?"
-    puts "Type the number of your choice"
+    puts "Type the name of your choice"
     input = gets.strip.downcase
     puts input
     case input #case statement 
       when "cookies"
         puts "you selected cookies"
         #scrape the cookies page
-        #how to call the scraper method 
         url = "https://proteinbakery.com/collections/cookies"
+        #how to call the scraper method 
         categories = Dessert::Scraper.scrape_categories(url)
-        puts categories[1].name 
-        puts categories[1].url 
+        puts categories[0].name 
+        puts categories[0].url 
       when "brownies" 
         puts "you selected brownies"
         #scrape the brownies page
--- lib/dessert/scraper.rb
+++ lib/dessert/scraper.rb
@@ -1,15 +1,18 @@
 class Dessert::Scraper
   
   def self.scrape_categories(url)
-    #cookies = webpage.css("div.info"), need to look at this one again
+    #cookies = webpage.css("div.info")
     #open the url and scrape all the categories
     #must include require "nokogiri"
     
     webpage = Nokogiri::HTML(open(url)) #opens webpage
-    name = webpage.css("span.title")[0].text #returns "Chocolate Chocolate Chip Cookie"
+    section = webpage.css("div.info")
     array_of_links = webpage.css("span.title")
+    name = webpage.css("span.title")[0].text #returns "Chocolate Chocolate Chip Cookie"
     
+      
     array_of_links.map do |link|
+      binding.pry
       Dessert::Category.new(link.text, link.attributes["href"].value) 
       #link.attributes["href"].value, will give link
       #return value will now be an array of objects because .map is used 
