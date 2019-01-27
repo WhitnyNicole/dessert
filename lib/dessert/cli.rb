91c9afda7d28b0d14ab5c656e3d77a80f65d2108
Whitny Edwards <wne4e2@gmail.com>
2019-01-27 17:52:43 +0000

    add def scrape_teams and def list_categories

--- lib/dessert/cli.rb
+++ lib/dessert/cli.rb
@@ -8,21 +8,15 @@ class Dessert::CLI
     puts "Team, Schedule, Standings?"
     puts "Type the item of your choice"
     input = gets.strip.downcase
-    puts input
     case input #case statement 
       when "team"
         puts "These are the NBA teams: "
-        #scrape the teams page
-        url = "http://www.nba.com/teams"
-        #how to call the scraper method 
-        Dessert::Scraper.scrape_categories(url) #should make objects
-        categories = Dessert::Category.all #array of objects
         
-        puts "Choose the team you want more information on"
+        scrape_teams
+        list_categories
+       
         
-        categories.each.with_index(0) do |category, index|
-          puts "#{index}. #{category.name}"  #printing out the category names
-        end 
+        
         
       when "schedule" 
         puts "you selected schedule"
@@ -38,4 +32,20 @@ class Dessert::CLI
         puts "Invalid"
     end 
   end 
+  
+  def scrape_teams #this method is calling my scraper class to scrape
+    #scrape the teams page
+    url = "http://www.nba.com/teams"
+    #how to call the scraper method 
+    Dessert::Scraper.scrape_categories(url) #should make objects
+  end 
+  
+  def list_categories
+    categories = Dessert::Category.all #array of objects
+        puts "Choose the team you want more information on"
+        
+        categories.each.with_index(0) do |category, index|
+          puts "#{index}. #{category.name}"  #printing out the category names
+        end 
+      end 
 end 
