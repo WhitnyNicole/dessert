8dea11db7633e27adf5436dc52c05453dd3b5a44
Whitny Edwards <wne4e2@gmail.com>
2019-01-29 01:15:02 +0000

    adding scrape_calendar method in scraper file

--- lib/nba/scraper.rb
+++ lib/nba/scraper.rb
@@ -1,7 +1,6 @@
 class NBA::Scraper
   
   def self.scrape_categories(url)
-    #section.css("span.title")[0].text returns "chocolate chocolate chip cookie"
     #open the url and scrape all the categories
     #must include require "nokogiri"
     
@@ -18,6 +17,20 @@ class NBA::Scraper
     #using .map return value is an array of objects (all the teams)
   
   end 
+  
+    def self.scrape_calendar(url) 
+        webpage = Nokogiri::HTML(open(url)) #opens webpage
+        section = webpage.css("div.team__list_wrapper") #list of teams
+ 
+    
+        array_of_teams = section.css("div.team__list a") #returns team name
+    
+    
+        array_of_teams.map do |team|
+        NBA::Category.new(team.text, team.attributes["href"].value)
+    end 
+  end 
+  
   
   def self.scrape_items(category) #category is representing an object
     webpage = Nokogiri::HTML(open(category.url)) #opens webpage
