91d90f8c708436802ec1cca4826ba6a7a9805034
Whitny Edwards <wne4e2@gmail.com>
2019-01-30 02:06:12 +0000

    removing erroneous methods

--- lib/nba/scraper.rb
+++ lib/nba/scraper.rb
@@ -3,30 +3,15 @@ class NBA::Scraper
   def self.scrape_categories(url)
     #open the url and scrape all the categories
     #must include require "nokogiri"
-    
     webpage = Nokogiri::HTML(open(url)) #opens webpage
     section = webpage.css("div.team__list_wrapper") #list of teams
- 
-    
     array_of_teams = section.css("div.team__list a") #returns team name
-    
-    
     array_of_teams.map do |team|
     NBA::Category.new(team.text, team.attributes["href"].value)
     end 
     #using .map return value is an array of objects (all the teams)
   
-  end 
-  
-    # def self.scrape_calendar(url) 
-    #     webpage = Nokogiri::HTML(open(url)) #opens webpage
-    #     binding.pry
-    #     section = webpage.css("div.article__detail--header-wrapper") 
-    #     array_of_news = section.css("div.article__detail--header") #returns team name
-    #     # array_of_teams.map do |team|
-    #     # NBA::Category.new(team.text, team.attributes["href"].value)
-    # end 
-  
+  end
   
   
   def self.scrape_items(category) #category is representing an object
@@ -39,8 +24,9 @@ class NBA::Scraper
     
   def self.scrape_players(url) #class method 
      webpage = Nokogiri::HTML(open(url)) 
-     players = webpage.css("section.nba-player-index__trending-item.small-4.medium-3.large-2.team-atl-hawks")
+     players = webpage.css("p.nba-player-index__name")
      players.each do |player|
+       player.name
        text = "Current player is "
        puts text 
    end 
