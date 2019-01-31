3818e16619c6d3a2a5378affd8519f553bbb8134
Whitny Edwards <wne4e2@gmail.com>
2019-01-31 01:36:57 +0000

    fixing broken method

--- lib/nba/players.rb
+++ lib/nba/players.rb
@@ -1,18 +1,18 @@
 class NBA::Players
   
-   attr_accessor :name, :team
+   attr_accessor :name
   
   @@all = []
   
-  def initialize(name)
-    @name = name
-    @players = []
-    @@all << self #remembering the object
-  end 
+#   def initialize(name)
+#     @name = name
+#     @players = []
+#     @@all << self #remembering the object
+#   end 
   
-  def self.all
-    @@all
-  end 
-end 
+#   def self.all
+#     @@all
+#   end 
+# end 
   
- 
+ end
--- lib/nba/scraper.rb
+++ lib/nba/scraper.rb
@@ -20,15 +20,29 @@ class NBA::Scraper
     players.each do |nodeset|
     NBA::Players.new(nodeset.text)
       puts "\n#{nodeset.text}" #player name
-      
     #everything works above here
     
-    schedule_link = webpage.css("team-info-stats")[0].next_element.css("a").attr("href").value #-->this returns link to hawks schedule
-    puts schedule_link
-    NBA::Team.new(team.text, team.attributes["href"].value)
+  #   schedule_link = webpage.css("team-info-stats")[0].next_element.css("a").attr("href").value #-->this returns link to hawks schedule
+  #   puts schedule_link
+  #   NBA::Team.new(team.text, team.attributes["href"].value)
   end 
 end 
 end 
+
+# def self.scrape_all_players(category) #category is representing an object
+#     webpage = Nokogiri::HTML(open(category.url)) #opens webpage the url is for the specific team
+#     players = webpage.css("p.nba-player-index__name")
+#     players.each do |nodeset|
+#     NBA::Players.new(nodeset.text)
+#     player = NBA::Players.new
+#     schedule_link = webpage.css("team-info-stats")[0].next_element.css("a").attr("href").value #-->this returns link to hawks schedule
+    
+#     team.add_player(player)
+#   end 
+# end 
+# end 
       
       # schdule = webpage.css("team-info-stats")[0].next_element.css("a").text #see full scheudle #don't need this 
+  
+  
   
