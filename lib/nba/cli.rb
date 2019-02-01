e478ab876758fbd44f379d64defdfb88e5c0ea86
Whitny Edwards <wne4e2@gmail.com>
2019-02-01 03:41:53 +0000

    adding color

--- lib/nba/cli.rb
+++ lib/nba/cli.rb
@@ -3,24 +3,24 @@
 class NBA::CLI 
   
   def start #instance method
-    puts "Welcome to the NBA.com!"
+    puts "Welcome to the NBA.com!".colorize(:blue)
     menu
   end 
   
   def menu
-    puts "\nWould you like to see the current teams?"
-    puts "\nPlease type yes or exit"
+    puts "\nWould you like to see the current teams?".colorize(:blue)
+    puts "\nPlease type yes or exit".colorize(:red)
     input = gets.strip.downcase
     case input #case statement 
       when "yes"
-        puts "\nThese are the current NBA teams: "
+        puts "\nThese are the current NBA teams: ".colorize(:blue)
         scrape_teams
         list_teams
         choose_team
       when "exit"
-        puts "\nGoodbye"
+        puts "\nGoodbye".colorize(:blue)
       else
-        puts "\nSorry, I didn't understand that input"
+        puts "\nSorry, I didn't understand that input".colorize(:blue)
         menu
     end 
   end 
@@ -32,7 +32,7 @@ class NBA::CLI
     end 
       
    def choose_team
-     puts "\nChoose a team by selecting a number 1-30:" #adding a line break by using \n
+     puts "\nChoose a team by selecting a number 1-30:".colorize(:blue) #adding a line break by using \n
      input = gets.strip.to_i #strip removes whitespace on both sides
      max = NBA::Team.all.length
      if input.between?(1,max)
@@ -50,9 +50,9 @@ class NBA::CLI
      NBA::Scraper.scrape_all_items(team) # "category" returns object ID, name and team url and scraper method returns player names
      team.items.each.with_index(1) do |item, index|
        #prints out information on each team --> players and schedule
-       puts "\nClick here to see the team schedule for the #{team.name}: #{item.schedule_link}"
-        puts "\nHere are the players for the #{team.name}: "
-       puts "\n#{index}. #{item.players}"
+       puts "\nClick here to see the team schedule for the #{team.name}: #{item.schedule_link}".colorize(:blue)
+        puts "\nHere are the players for the #{team.name}: ".colorize(:blue) 
+       puts "\n#{index}. #{item.players}".colorize(:red)
      end 
      second_menu
    end 
@@ -75,9 +75,9 @@ class NBA::CLI
     elsif input == "start"
       menu
     elsif input == "exit"
-      puts "\nGoodbye!"
+      puts "\nGoodbye!".(:blue)
     else
-      puts "\nSorry I don't understand"
+      puts "\nSorry I don't understand".(:blue)
       second_menu
     end
   end 
