7a0db1a8d7f4a2a1570526e1d01dc5e115693a95
Whitny Edwards <wne4e2@gmail.com>
2019-02-05 13:30:16 +0000

    adding notes to team file

--- lib/nba/team.rb
+++ lib/nba/team.rb
@@ -7,12 +7,12 @@ class NBA::Team
   #reader/getter returns information stored in an instance such as @name
   #writer/setter creates name, such as name=
     
-  @@all = []
+  @@all = [] #class variable that represents saving every instance of a class (i.e all the items for all of the teams)
   
   def initialize(name, url)
     @name = name
     @url = "http://www.nba.com" + url 
-    @scouting_report = []
+    @scouting_report = [] #every item object for that one instance of a team object
     @@all << self #remembering the object
   end 
   
@@ -21,7 +21,7 @@ class NBA::Team
   end 
   
   def add_items(items) #items is an object
-    self.scouting_report << items 
-    items.team = self 
+    self.scouting_report << items #adding an instance of the items object to the scouting_report array 
+    items.team = self #tell the team object about it's items 
   end 
 end 
